1c138127a22e994b9a62141b27a60a5feec7618c
Brock Norton <bbrocknorton@gmail.com>
2015-03-12 20:44:46 -0600

    All math done, will work on displaying pie chart of actual costs and displaying hourly wage commuting, IRS/actual/avg cost to drive + IRS/actual/avg & wage cost in bar format using d3 with scales

--- controller.js
+++ controller.js
@@ -1,7 +1,8 @@
 var app = angular.module("CommuteCalculator");
 
-app.controller("CommuteController", function($scope) {
 
+app.controller("CommuteController", function($scope) {
+	var actual = $scope.carGasAns + $scope.tire + $scope.oil + $scope.maint + $scope.ins;
 	$scope.carGas = function() {
 		$scope.carGasAns = $scope.dollarPerGallonOfGas / $scope.mpgOfCar;
 	};
@@ -31,80 +32,143 @@ app.controller("CommuteController", function($scope) {
 		$scope.hoursRound = $scope.minsRound / 60;
 	};
 
-	$scope.eightHourRounds = function() {
-
+	$scope.eightHourRound = function() {
+		$scope.eightHourRound = $scope.hoursRound / 8;
 	};
 
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
+	$scope.fortyHourRound = function() {
+		$scope.fortyHourRound = $scope.eightHourRound / 40;
+	};
 
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
+	$scope.hourlyWageRound = function() {
+		$scope.hourlyWageRound = $scope.avgHourlyWage * $scope.hoursRound;
+	};
 
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
+	$scope.milesRound = function() {
+		$scope.milesDrivenRound = $scope.milesToWork * 2;
+	};
 
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
+	$scope.irsRound = function() {
+		$scope.irsRound = $scope.milesDrivenRound * .51;
+	};
 
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
+	$scope.actualRound = function() {
+		var actual = $scope.carGasAns + $scope.tire + $scope.oil + $scope.maint + $scope.ins;
+		$scope.actualRound = $scope.milesDrivenRound * actual;
+	};
 
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
-	$scope. = function() {};
+	$scope.avgRound = function() {
+		$scope.avgRound = ($scope.irsRound + $scope.actualRound) / 2;
+	};
+
+	$scope.wageIrsRound = function() {
+		$scope.wageIrsRound = $scope.irsRound + $scope.hourlyWageRound;
+	};
+
+	$scope.wageActualRound = function() {
+		$scope.wageActualRound = $scope.actualRound + $scope.hourlyWageRound;
+	};
+
+	$scope.wageAverageRound = function() {
+		$scope.wageAverageRound = $scope.avgRound + $scope.hourlyWageRound;
+	};
+
+	$scope.minsWeek = function() {
+		$scope.minsWeek = $scope.minsRound * 5;
+	};
+
+	$scope.hoursWeek = function() {
+		$scope.hoursWeek = $scope.hoursRound * 5;
+	};
+
+	$scope.eightHourWeek = function() {
+		$scope.eightHourWeek = $scope.eightHourRound * 5;
+	};
+
+	$scope.fortyHourWeek = function() {
+		$scope.fortyHourWeek = $scope.fortyHourRound * 5;
+	};
+
+	$scope.hourlyWageWeek = function() {
+		$scope.hourlyWageWeek = $scope.hourlyWageRound * 5;
+	};
+
+	$scope.milesDrivenWeek = function() {
+		$scope.milesDrivenWeek = $scope.milesDrivenRound * 5;
+	};
+
+	$scope.irsWeek = function() {
+		$scope.irsWeek = $scope.milesDrivenWeek * .51;
+	};
+
+	$scope.actualWeek = function() {
+		var actual = $scope.carGasAns + $scope.tire + $scope.oil + $scope.maint + $scope.ins;
+		$scope.actualWeek = $scope.milesDrivenWeek * actual;
+	};
+
+	$scope.avgWeek = function() {
+		$scope.avgWeek = ($scope.irsWeek + $scope.actualWeek) / 2;
+	};
+
+	$scope.wageIrsWeek = function() {
+		$scope.wageIrsWeek = $scope.hourlyWageWeek + $scope.irsWeek;
+	};
+
+	$scope.wageActualWeek = function() {
+		$scope.wageActualWeek = $scope.hourlyWageWeek + $scope.actualWeek;
+	};
+
+	$scope.wageAverageWeek = function() {
+		$scope.wageAverageWeek = $scope.hourlyWageWeek + $scope.avgWeek;
+	};
+
+	$scope.minsTen = function() {
+		$scope.minsTen = $scope.minsWeek * 520;
+	};
+
+	$scope.hoursTen = function() {
+		$scope.hoursTen = $scope.hoursWeek * 520;
+	};
+
+	$scope.eightHourTen = function() {
+		$scope.eightHourTen = $scope.eightHourWeek * 520;
+	};
+
+	$scope.fortyHourTen = function() {
+		$scope.fortyHourTen = $scope.fortyHourWeek * 520;
+	};
+
+	$scope.hourlyWageTen = function() {
+		$scope.hourlyWageTen = $scope.hourlyWageWeek * 520;
+	};
+
+	$scope.milesDrivenTen = function() {
+		$scope.milesDrivenTen = $scope.milesDrivenWeek * 520;
+	};
+
+	$scope.irsTen = function() {
+		$scope.irsTen = $scope.milesDrivenTen * .51;
+	};
+
+	$scope.actualTen = function() {
+		var actual = $scope.carGasAns + $scope.tire + $scope.oil + $scope.maint + $scope.ins;
+		$scope.actualTen = $scope.milesDrivenTen * actual;
+	};
+
+	$scope.avgTen = function() {
+		$scope.avgTen = ($scope.irsTen + $scope.actualTen) / 2;
+	};
+
+	$scope.wageIrsTen = function() {
+		$scope.wageIrsTen = $scope.hourlyWageTen + $scope.irsTen;
+	};
+
+	$scope.wageActualTen = function() {
+		$scope.wageActualTen = $scope.hourlyWageTen + $scope.actualTen;
+	};
+
+	$scope.wageAverageTen = function() {
+		$scope.wageAverageTen = $scope.hourlyWageTen + $scope.avgTen;
+	};
 
 });
